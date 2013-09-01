fd1e209201ea2d613342be7dd561c8b521af30b7
Charles Sanquer <charles.sanquer@gmail.com>
2013-09-01 13:12:03 +0200

    improve git getversion script

--- tools/symfony2/getversion.sh
+++ tools/symfony2/getversion.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
-config=app/config/parameters.yml
+defaultConfig=app/config/parameters.yml
 versionKeyword=version
 versionDateKeyword=version_date
+
+version=
+versionDate=
+
+if [ $# -gt 0 ]; then
+    configFiles="$@"
+else
+    configFiles=$defaultConfig
+fi
 
 if ! type "git" > /dev/null 2>&1; then
   echo unable to retrieve version, git is not installed
 else
     if [ -d .git ]; then
         versionDate=`git log -1 --pretty=format:'%ci' --date=local`
-        version=`git describe --tags 2> /dev/null`
-
         if [ $? != 0 ]; then
+            versionDate=`date +"%F %T %z"`
+        fi
+        
+        #only based on tags
+        version=`git describe --tags 2> /dev/null`
+        if [ $? != 0 ]; then
+            # refs + commit short hash
             #version=`git describe --all 2> /dev/null`
             #hash=`git log -1 --pretty=format:%h 2> /dev/null`
             #version=$version-$hash
+            
+            # only commit short ash
             version=`git log -1 --pretty=format:%h 2> /dev/null`
         fi
     else 
-        version=
-        versionDate=
+        echo this directory is not a git repository
     fi
 
-    if [ -n "$version" ]; then
+    if [ -n "$version" -a -n "$versionDate" ]; then
         echo Version : $version
+        echo Version Date : $versionDate
 
-        if [ -f $config ]; then
-            sed  -i 's/\('"$versionKeyword"':\)\s*.*$/\1 '"\""''"$version"''"\""'/' $config
-        fi
-
-        if [ -n "$versionDate" ]; then
-            echo Version Date : $versionDate
-
-            if [ -f $config ]; then
-                sed  -i 's/\('"$versionDateKeyword"':\)\s*.*$/\1 '"\""''"$versionDate"''"\""'/' $config
+        for file in $configFiles
+        do
+            if [ -f $file ]; then
+                sed  -i 's/\('"$versionKeyword"':\)\s*.*$/\1 '"\""''"$version"''"\""'/' $file
+                sed  -i 's/\('"$versionDateKeyword"':\)\s*.*$/\1 '"\""''"$versionDate"''"\""'/' $file
             fi
-        fi
+        done
     fi
 fi
