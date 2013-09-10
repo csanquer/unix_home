ffa2775d82d7187035a72f98a6868e902d4c70d9
Charles Sanquer <charles.sanquer@gmail.com>
2013-09-10 18:03:59 +0200

    fix create tag script when we are already on the latest tag

--- tools/symfony2/create_tag.sh
+++ tools/symfony2/create_tag.sh
@@ -99,56 +99,65 @@ arg1=$1
 ########################################
 
 currentTag=`git describe --tags --abbrev=0 2> /dev/null`
-tag=$(echo $currentTag | awk 'match($0, /[0-9]+(\.[0-9]+)+/) { print substr( $0, RSTART, RLENGTH )}')
+currentFullTag=`git describe --tags 2> /dev/null`
 
-echo current Tag : $currentTag
+if [ "$currentTag" != "$currentFullTag" -o -z "$currentTag" ]; then
+    tag=$(echo $currentTag | awk 'match($0, /[0-9]+(\.[0-9]+)+/) { print substr( $0, RSTART, RLENGTH )}')
+    
+    echo current Full Tag : $currentFullTag
+    echo current Tag : $currentTag
+    
+    if [[ -n "$tag" ]] ; then 
+        if [ "$field" != 'patch' -a "$field" != 'minor' -a "$field" != 'major' ] ; then 
+            PS3="Please choose a version field to increment : "
+            select option in 'Patch Number (v0.0.x)' 'Minor Version (v0.x.0)' 'Major Version (vx.0.0)'
+            do
+                case "$option" in
+	                'Patch Number (v0.0.x)')
+	                    field="patch"
+                        break
+		                ;;
+                 
+	                'Minor Version (v0.x.0)')
+	                    field=minor
+	                    break
+	                    ;;
+	                'Major Version (vx.0.0)')
+	                    field=major
+	                    break
+		                ;;
+                esac
+            done
+        fi
 
-if [[ -n "$tag" ]] ; then 
-    if [ "$field" != 'patch' -a "$field" != 'minor' -a "$field" != 'major' ] ; then 
-        PS3="Please choose a version field to increment : "
-        select option in 'Patch Number (v0.0.x)' 'Minor Version (v0.x.0)' 'Major Version (vx.0.0)'
-        do
-            case "$option" in
-	            'Patch Number (v0.0.x)')
-	                field="patch"
-                    break
-		            ;;
-             
-	            'Minor Version (v0.x.0)')
-	                field=minor
-	                break
-	                ;;
-	            'Major Version (vx.0.0)')
-	                field=major
-	                break
-		            ;;
-            esac
-        done
+        case "$field" in
+	        'patch')
+                nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+0 "." $2+0 "." $3+1;}')
+		        ;;
+         
+	        'minor')
+                nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+0 "." $2+1 ".0";}')
+		        ;;
+         
+	        'major')
+                nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+1 ".0.0";}')
+		        ;;
+        esac
+    else
+        nextTag=$(echo $defaultTag | awk 'match($0, /[0-9]+(\.[0-9]+)+/) { print substr( $0, RSTART, RLENGTH )}')
     fi
 
-    case "$field" in
-	    'patch')
-            nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+0 "." $2+0 "." $3+1;}')
-		    ;;
-     
-	    'minor')
-            nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+0 "." $2+1 ".0";}')
-		    ;;
-     
-	    'major')
-            nextTag=$(echo $tag | awk 'BEGIN{FS="."} { print $1+1 ".0.0";}')
-		    ;;
-    esac
+    nextTag=v$nextTag
+
+    echo next Tag : $nextTag
+
+    if [[ $createTag -eq 1 ]] ; then
+        echo creating tag $nextTag in git repository on commit $commit
+        git tag $nextTag $commit -m "$nextTag $field version"
+    fi
 else
-    nextTag=$(echo $defaultTag | awk 'match($0, /[0-9]+(\.[0-9]+)+/) { print substr( $0, RSTART, RLENGTH )}')
+    echo you are already on the latest tag $currentTag
 fi
 
-nextTag=v$nextTag
 
-echo next Tag : $nextTag
-
-if [[ $createTag -eq 1 ]] ; then
-    echo creating tag $nextTag in git repository on commit $commit
-    git tag $nextTag $commit -m "$nextTag $field version"
-fi
 
